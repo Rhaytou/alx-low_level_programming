55979ce50cc813013eeacb8aa151fef2702b318f
Slimane Rhaytou <rhaytouslimane@gmail.com>
2023-09-07 07:49:59 -0700

    Commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Main function declaration
- * Prints the phrase with proper grammar
- * Return 0
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
