a2f0cc166fe83f99480bf091bda92df11eecea6f
Slimane Rhaytou <rhaytouslimane@gmail.com>
2023-09-07 07:44:34 -0700

    Commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * Main function declaration
+ * Prints the phrase with proper grammar
+ * Return 0
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
