38310969e09c8f319bd4c15b523c3e722af369b0
Slimane Rhaytou <rhaytouslimane@gmail.com>
2023-09-12 09:27:01 -0700

    Commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followd by a new line
+ * Return: Alwys 0 (Success)
+ */
+
+int main(void)
+{
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
